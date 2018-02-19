cc243c516bba64fd5a20f18d4f2aebdc8e1b2f5f
walkerflocker <1042.aw@gmail.com>
2018-02-18 20:47:43 -0700

    url handling all cases

--- src/hls_viewer/urls.cljs
+++ src/hls_viewer/urls.cljs
@@ -3,6 +3,7 @@
 
 (defn constructUrl [manifestUrl text]
   (cond (re-matches #"^http.*" text) text
-        (= (first text) "/") "aylmao"
+        (= (first text) "/")
+         (str (aget (new js/URL manifestUrl) "origin") text)
         :else (str (cljstr/join "/"
               (pop (cljstr/split manifestUrl "/"))) "/" text)))
